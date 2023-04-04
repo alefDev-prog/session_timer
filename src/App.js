be765247ebcf9d93dfe2c9d00770569d5ac30031
alefCS50 <allan.efendic@icloud.com>
2023-04-04 15:21:36 +0200

    fix: accordion-animations

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ function App() {
   const showSettings = () => {
     toggleSettings(!settings);
 
+    const title = document.querySelector("#settings-title");
+    if(settings)title.style.borderRadius = "2rem";
+    else title.style.borderRadius = "2rem 2rem 0 0";
+
     //spinning gear
     const gear = document.getElementById("gear");
     gear.classList.add("spinning");
@@ -29,9 +33,11 @@ function App() {
       <Timer class="tim"/>
         <div className='container' id="settings-container">
         
-            <h2 id="settings-title" onClick={() => showSettings()}><FontAwesomeIcon id="gear" icon={faGear} /></h2>
-            <div id="settings-wrapper" style={settings ? {height:200,
-    paddingTop: "1.5rem"}: {}}>
+            <h2 id="settings-title" onClick={() => showSettings()}>
+              <FontAwesomeIcon id="gear" icon={faGear} />
+            </h2>
+            <div id="settings-wrapper" style={settings ? 
+            {height:200, paddingTop: "1.5rem"}: {backgroundColor: "transparent"}}>
               <SessionControl />
               <BreakControl/>
             </div>
